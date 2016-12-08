d5b0f88334e1659fe314be0255dc07c4e4bf01ea
rajini <rajini@192.168.1.62>
2016-12-08 19:48:13 +0530

    added view resolver

--- src/main/java/com/daily/controllers/DailyExpenseTrackerController.java
+++ src/main/java/com/daily/controllers/DailyExpenseTrackerController.java
@@ -1,15 +1,21 @@
 package com.daily.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class DailyExpenseTrackerController {
 	
 	@RequestMapping("/home")
-	public String homePage(){		
+	@ResponseBody
+	public ModelAndView homePage(){		
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("test");
 		
-	 return "index";
+	 return mv;
 	}
+
 
 }
---
+++ src/main/java/com/daily/web/WebMVCAdapter.java
@@ -0,0 +1,34 @@
+package com.daily.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+
+public class WebMVCAdapter extends WebMvcConfigurerAdapter {
+	
+	   @Bean
+	    public ViewResolver getViewResolver() {
+	        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	        resolver.setPrefix("views/");
+	        resolver.setSuffix(".html");
+	        return resolver;
+	    }
+	   @Override
+	    public void configureDefaultServletHandling(
+	            DefaultServletHandlerConfigurer configurer) {
+	        configurer.enable();
+	    } 
+	   @Override
+	   public void addViewControllers(ViewControllerRegistry registry) {
+	     registry.addViewController("/").setViewName("forward:/index.html");
+	   }
+
+}
