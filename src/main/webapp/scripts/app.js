379bc81fef05567d90f08f53ad624fca257115b2
Sasmita4 <sasmita.maharana555@gmail.com>
2017-01-25 22:30:22 +0530

    First commit

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -11,6 +11,7 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 	    .state('main.addFood', {url:'/addFood',  templateUrl: 'views/food/addFood.html', controller: 'addFoodController'})
 	    .state('main.addTransportation', {url:'/addTransportation',  templateUrl: 'views/transportation/addTransportation.html', controller: 'addTransportationController'})
 	    .state('main.addMiscellaneous', {url:'/addMiscellaneous',  templateUrl: 'views/miscellaneous/addMiscellaneous.html', controller: 'addMiscellaneousController'})
+	    .state('main.createCategory', {url:'createCategory', templateUrl: 'views/category/createCategory.html', controller: 'createCategoryController'})
 	    .state('viewFood', { url: '/viewFood',templateUrl: 'views/food/viewFood.html',controller:'viewFoodController'})
 	    .state('viewTransportation', { url: '/viewTranportation',templateUrl: 'views/tranportation/viewTranportation.html',controller:'viewTranportationController'})
 	    .state('main.report', {url:'/searchReport',templateUrl: 'views/reports/report.html',controller:'reportController'})
---
+++ src/main/webapp/scripts/js/category/createCategoryController.js
@@ -0,0 +1,4 @@
+app.controller('createCategoryController',['$scope','$state','$http',function(){
+	
+	
+}]);
